3718c40a5b0de61cacc83c2957e3b707fc1e5d35
David Mills <dmills@atout-serenite.com>
2018-02-27 10:52:15 +0100

    Reverting webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,91 +1,42 @@
-
-const path = require('path'),
+var path = require('path'),
 webpack = require('webpack'), // Da bundling modules!
 NpmInstallPlugin = require('npm-install-webpack-plugin'), // Install client dependencies automatically!
-merge = require('webpack-merge'), // Merge together configurations!
-HtmlWebpackPlugin = require('html-webpack-plugin');
+merge = require('webpack-merge'); // Merge together configurations!
 
 const PATHS = {
-  src: path.join(__dirname, 'src'),
-  build: path.join(__dirname, 'dist')
+  js: path.join(__dirname, './js/'),
+  build: path.join(__dirname, './build')
 };
-
-const moduleName = 'recordy';
 
 const TARGET = process.env.npm_lifecycle_event;
 
 const COMMON_CONFIGURATION = {
   entry: {
-    [moduleName]: path.join(PATHS.src, 'index.js'),
-    [moduleName + '.min']: path.join(PATHS.src, 'index.js')
+    app: PATHS.js
   },
   resolve: {
-    extensions: ['.js'], // Resolve these extensions
+    extensions: ['', '.js'], // Resolve these extensions
   },
   output: {
     path: PATHS.build,
-    filename: '[name].js',
+    filename: 'bundle.js',
     libraryTarget: 'umd',
-    library: moduleName,
-    umdNamedDefine: true
+    library: 'Recordy'
   },
   module: {
-    rules: [
+    loaders: [
       {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        include: PATHS.src,
-        options: {
-          cacheDirectory: true
-        }
-      },
-      {
-        test: /\.(jpe?g|png|gif|svg|wav|mp3)$/i,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              hash: 'sha512',
-              digest: 'hex',
-              name: '[hash].[ext]'
-            }
-          },
-          {
-            loader: 'image-webpack-loader',
-            options: {
-              bypassOnDebug: true
-            }
-          }
-        ],
-        include: PATHS.src
+       test: /\.js$/,
+       loaders: ['babel?cacheDirectory'],
+       include: PATHS.js
       }
     ]
-  },
-  plugins: [
-    new webpack.DefinePlugin({
-      ENVIRONMENT: JSON.stringify(TARGET === 'start:dev' ? 'development' : 'production')
-    }),
-    new webpack.LoaderOptionsPlugin({
-      test: /\.(jpe?g|png|gif|svg)$/i,
-      options: {
-        imageWebpackLoader: {
-          gifsicle: {
-            interlaced: false
-          },
-          optipng: {
-            optimizationLevel: 7
-          }
-        }
-      }
-    }),
-    new HtmlWebpackPlugin({
-      excludeAssets: [/\.min\.js$/],
-      title: moduleName
-    })
-  ]
+  }
 };
 
 switch(TARGET) {
+  // Which procedure was started?
+  default:
   case 'start:dev': {
     module.exports = merge(COMMON_CONFIGURATION, {
       devServer: {
@@ -93,10 +44,14 @@ switch(TARGET) {
         historyApiFallback: true,
         hot: true,
         inline: true,
+        progress: true,
         stats: 'errors-only'
       },
       plugins: [
-        new webpack.HotModuleReplacementPlugin()
+        new webpack.HotModuleReplacementPlugin(),
+        new NpmInstallPlugin({
+          save: true
+        })
       ],
       devtool: 'eval-source-map'
     });
@@ -111,8 +66,9 @@ switch(TARGET) {
           }
         }),
         new webpack.optimize.UglifyJsPlugin({
-          include: /\.min\.js$/
-        })
+          compress: { warnings: false }
+        }),
+        new webpack.optimize.DedupePlugin()
       ]
     });
   }
